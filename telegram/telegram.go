c2d0bfb1faadd03195474201e48f5c9ff05100e8
ericsource <ericenter@gmail.com>
2023-02-22 11:38:06 +0800

    1. https 跳过证书验证

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -1,21 +1,27 @@
 package telegram
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/869413421/wechatbot/config"
+	"github.com/869413421/wechatbot/gtp"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
-	"github.com/869413421/wechatbot/config"
-	"github.com/869413421/wechatbot/gtp"
 )
 
 func Run() {
 	// 设置代理http或sock5
 	proxyUrl, err := url.Parse(config.LoadConfig().TelegramProxy)
 	fmt.Println("proxy:", proxyUrl)
-	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}} //使用代理
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		//使用代理
+		Proxy: http.ProxyURL(proxyUrl),
+	}
+	myClient := &http.Client{Transport: transport}
 	APIEndpoint := "https://api.telegram.org/bot%s/%s"
 	bot, err := tgbotapi.NewBotAPIWithClient(config.LoadConfig().TelegramToken, APIEndpoint, myClient)
 	if err != nil {
