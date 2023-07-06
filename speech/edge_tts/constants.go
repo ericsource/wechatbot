ed298d147f2960250022ede71e6d89faf1b35e88
ericsource <ericenter@gmail.com>
2023-07-06 16:51:51 +0800

    1. list voices

---
+++ speech/edge_tts/constants.go
@@ -0,0 +1,7 @@
+package edge_tts
+
+const (
+	TrustedClientToken = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+	WssUrl             = "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=" + TrustedClientToken
+	VoiceList          = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=" + TrustedClientToken
+)
