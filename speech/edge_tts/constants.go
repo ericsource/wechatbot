f515806e48a902761791edc7c1a0edcb042c1e62
ericsource <ericenter@gmail.com>
2023-09-12 01:30:49 +0800

    1. trusted client

--- speech/edge_tts/constants.go
+++ speech/edge_tts/constants.go
@@ -1,7 +1,8 @@
 package edge_tts
 
 const (
-	TrustedClientToken = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
-	WssUrl             = "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=" + TrustedClientToken
-	VoiceList          = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=" + TrustedClientToken
+	TrustedClientToken  = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+	TrustedClientToken2 = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+	WssUrl              = "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=" + TrustedClientToken
+	VoiceList           = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=" + TrustedClientToken
 )
